e332b523ee84ed9565627ef80addc6b63cbc3a80
mc-agavel27 <mc_agavel27@mcjvs2.org>
2025-10-15 09:59:19 -0400

    inital commit

---
+++ js01/About Me/about-me.js
@@ -0,0 +1,15 @@
+//Sends the alert
+console.log("Script running");
+alert("Welcome to my website!");
+
+//Writes my name as a heading
+console.log("Writing to document");
+document.write("<h1>Adam Gavel</h1>");
+
+//Writes three paragraphs (about myself)
+document.write("<p1>I think programming is fun. No matter what I'm doing... usually. Sometimes it gets repetitive. </p1> <br>");
+document.write("<p1>Music is my favorite thing, ever. Very cool, best form of expression. Art is a very close second. </p1> <br>");
+document.write("<p1>I don't know what else to put... but I like cats. So, that's pretty... pawsome... isn't it...</p1> <br>");
+
+//Sends a log, showing completion
+console.log("Script completed");
---
+++ js01/script.js
@@ -0,0 +1,3 @@
+//Adam Gavel
+console.log("External file loaded!");
+document.write("<p>I have learned... not a lot. Maybe how document.write works, but that's about it");
